910d958d17b083bf2156ef777dfc4b825e7c590b
Andre Torrez <andre@torrez.org>
2011-01-24 16:13:28 -0800

    Added source URL. Please work.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -12,14 +12,22 @@ mlkshk = {
         menuitem1.hidden = !gContextMenu.onImage;
     },
     subir : function() {
-        var strImgURL = gContextMenu.imageURL;
-        //myTabRef = getBrowser().addTab();
-        //getBrowser().selectedTab = myTabRef;
-        //var tabBr = getBrowser().getBrowserForTab(myTabRef);
-        //var doc = tabBr.contentDocument;
-        //doc.location.href = mlkshk.mlkshkurl + strImgURL;
+        var image_url = gContextMenu.imageURL;
+        source_query = ""
         
-        window.open(mlkshk.mlkshkurl + strImgURL, "Save an image.", "toolbar=yes,location=yes,directories=yes,status=yes,menubar=yes,scrollbars=yes,copyhistory=yes,resizable=yes");
+        source_url = getBrowser().contentWindow.location.href;
+        
+        if(source_url == image_url)
+        {
+          path_array = source_url.split('/');
+          source_url = path_array[0] + "//" + path_array[2];
+        }
+        
+        if (source_url) {
+            source_query = "&source_url=" + escape(source_url);
+        }
+
+        window.open(mlkshk.mlkshkurl + escape(image_url) + source_query, "Save an image.", "toolbar=yes,location=yes,directories=yes,status=yes,menubar=yes,scrollbars=yes,copyhistory=yes,resizable=yes");
     },
 };
 
